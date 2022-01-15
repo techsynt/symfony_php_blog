cadb26f2b6dc78ba6d0e27f367c875d857ab1f88
[techsynt] <[techsynt@gmail.com]>
2022-01-15 21:07:57 +0300

    added createpost action

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,10 +7,35 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Post;
 use App\Repository\PostRepository;
+use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
+    /**
+     * @Route("/newPost")
+     */
+    public function newPost(){
+        return $this->render('/blog/newPost.html.twig');
+    }
+
+    /**
+     * @Route("/api/post/create", name = "createPost", methods = {"POST"})
+     */
+    public function createPost(ManagerRegistry $doctrine, Request $request){
+        $entityManager = $doctrine->getManager();
+        $post = new Post();
+        $post->setTitle($request->request->get('title'));
+        $post->setContent($request->request->get('content'));
+        $post->setCreatedAt(new DateTimeImmutable('now'));
+        $entityManager->persist($post);
+        $entityManager->flush();
+        return $this->redirectToRoute("blog");
+        
+    }
+
+
     /**
      * @Route("/", name="blog")
      */
